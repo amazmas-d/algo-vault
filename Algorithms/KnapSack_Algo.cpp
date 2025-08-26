6c5cb3ada9dfa238d591226a1375ba0b11188b06
Azman Sikder <azmansikder143@gmail.com>
2025-08-26 09:59:58 +0600

    Add files via upload

---
+++ Algorithms/KnapSack_Algo.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findMax(int n1, int n2){
+   if(n1>n2) {
+      return n1;
+   } else {
+      return n2;
+   }
+}
+int knapsack(int W, int wt[], int val[], int n){
+   int K[n+1][W+1];
+   for(int i = 0; i<=n; i++) {
+      for(int w = 0; w<=W; w++) {
+         if(i == 0 || w == 0) {
+            K[i][w] = 0;
+         } else if(wt[i-1] <= w) {
+            K[i][w] = findMax(val[i-1] + K[i-1][w-wt[i-1]], K[i-1][w]);
+         } else {
+            K[i][w] = K[i-1][w];
+         }
+      }
+   }
+   return K[n][W];
+}
+int main(){
+   int val[5] = {70, 20, 50};
+   int wt[5] = {11, 12, 13};
+   int W = 30;
+   int len = sizeof val / sizeof val[0];
+   cout<<"Maximum Profit achieved with this knapsack: "<< knapsack(W, wt, val, len);
+}
